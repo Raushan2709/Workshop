5c3de0b4d42033672ad610f79058cec56d2b1a78
RAUSHAN KUMAR <160950026+Raushan2709@users.noreply.github.com>
2024-09-24 19:44:37 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,15 @@
+class cars:
+    def __init__(self):
+        self.brand="suire"
+        self.speed=154
+Cars=cars()
+Cars.brand      
+class cars:
+    def __init__(self,b,s):
+        self.brand=b
+        self.speed=s
+    def acceraletes(self):
+        print("your car top speed is :",self.speed)
+Cars=cars("suize",457)
+Cars.acceraletes()    
+
